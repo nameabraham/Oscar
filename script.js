946b27d9d3a7a5c618a8847ce12a0ceac3ad7d21
Ibraham <ebrahm772@gmail.com>
2025-03-22 14:52:23 -0700

    تحديث صفحة الأسئلة الشائعة وتحسين التنسيق

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 const mobileToggle = document.getElementById("mobile-toggle");
 
 mobileToggle.addEventListener("click", () => {
-  document.querySelector(".nav-items").classList.toggle("active");
+document.querySelector(".nav-items").classList.toggle("active");
 });
 
 const navLinks = document.querySelectorAll(".nav-item").forEach((navItem) => {
-  navItem.addEventListener("click", () => {
-    document.querySelector(".nav-items").classList.toggle("active");
-  });
+navItem.addEventListener("click", () => {
+document.querySelector(".nav-items").classList.toggle("active");
+});
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const features = document.querySelectorAll('.feature');
+const features = document.querySelectorAll('.feature');
 
-    features.forEach(feature => {
-        const images = feature.querySelectorAll('.feature-images img');
-        let currentIndex = 0;
+features.forEach(feature => {
+    const images = feature.querySelectorAll('.feature-images img');
+    let currentIndex = 0;
 
-        if (images.length > 0) {
-            images[0].classList.add('active'); 
-        }
+    if (images.length > 0) {
+        images[0].classList.add('active'); 
+    }
 
-        function changeImage() {
-            images[currentIndex].classList.remove('active'); 
-            currentIndex = (currentIndex + 1) % images.length; //   
-            images[currentIndex].classList.add('active'); //   
-        }
+    function changeImage() {
+        images[currentIndex].classList.remove('active'); 
+        currentIndex = (currentIndex + 1) % images.length; //   
+        images[currentIndex].classList.add('active'); //   
+    }
 
-        setInterval(changeImage, 3000);  
-    });
+    setInterval(changeImage, 3000);  
+});
 });
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  const aboutSection = document.querySelector("#about-us");
+const aboutSection = document.querySelector("#about-us");
 
-  function checkScroll() {
-      const sectionPosition = aboutSection.getBoundingClientRect().top;
-      const screenPosition = window.innerHeight / 1.3;
+function checkScroll() {
+  const sectionPosition = aboutSection.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.3;
 
-      if (sectionPosition < screenPosition) {
-          aboutSection.classList.add("show");
-      }
+  if (sectionPosition < screenPosition) {
+      aboutSection.classList.add("show");
   }
+}
 
-  window.addEventListener("scroll", checkScroll);
+window.addEventListener("scroll", checkScroll);
 });
 
 
